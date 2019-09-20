a076c4440859136dabd5051565fa5bfedd84f47f
jchen <1443616037@qq.com>
2019-09-20 15:01:30 +0800

    thread safe

---
+++ src/main/java/com/jchen/concurrency/Cp04/sync/CountExample02.java
@@ -0,0 +1,51 @@
+package com.jchen.concurrency.Cp04.sync;
+
+import com.jchen.concurrency.annoations.NotThreadSafe;
+import com.jchen.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class CountExample02 {
+
+    public static int clientTotal=5000;
+
+    public static int threadTotal=200;
+
+    public static volatile int count=0;
+
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService= Executors.newCachedThreadPool();
+
+        final Semaphore semaphore=new Semaphore(threadTotal);
+
+        final CountDownLatch countDownLatch=new CountDownLatch(clientTotal);
+
+        for (int i = 0; i <clientTotal ; i++) {
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                }catch (Exception e){
+                    log.error("execption",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}",count);
+    }
+
+    private static  void add(){
+//        单独加volatile 不行，count++不是原子操作,不适合计数场景
+        count++;
+    }
+}
